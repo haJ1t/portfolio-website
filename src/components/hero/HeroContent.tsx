f1ad769b0ab8614fcd4729350451d9997b3d957d
Your Name <your.email@example.com>
2025-12-27 22:18:29 +0000

    fixing typescript for deployment

--- src/components/hero/HeroContent.tsx
+++ src/components/hero/HeroContent.tsx
@@ -38,7 +38,7 @@ export default function HeroContent() {
       filter: 'blur(0px)',
       transition: {
         duration: 1.2,
-        ease: [0.6, 0.05, 0.01, 0.9],
+        ease: [0.6, 0.05, 0.01, 0.9] as const,
       },
     },
   };
